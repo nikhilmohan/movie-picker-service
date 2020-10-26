585365f8195d130d276575a06a93511679fd7ceb
nikhilmohan <nikmohan81@gmail.com>
2020-10-26 16:52:43 +0530

    initial commit

---
+++ src/main/java/com/nikhilm/hourglass/moviepickerservice/MoviePickerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nikhilm.hourglass.moviepickerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePickerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePickerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikhilm/hourglass/moviepickerservice/MoviePickerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilm.hourglass.moviepickerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePickerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
